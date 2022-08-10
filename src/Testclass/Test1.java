9cd6ea2bc5344133fb0ebfd36d71bf5fcd36f909
yogeshsapnar <110964746+yogeshsapnar@users.noreply.github.com>
2022-08-10 17:47:35 +0530

    First Commit.....

---
+++ src/Testclass/Test1.java
@@ -0,0 +1,20 @@
+package Testclass;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String hungry= "yes";
+		if(hungry == "yes")
+		{
+			System.out.println("Eat Smoething");
+			
+		}
+		else
+		{
+			System.out.println("Do work...");
+		}
+	
+	}
+
+}
